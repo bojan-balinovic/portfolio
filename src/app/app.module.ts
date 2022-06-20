0071cb32a4deb03765f043b43e7916351de8cca8
_Bojan_ <bojanbalinovi@gmail.com>
2022-06-20 22:44:30 +0200

    wip

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { BirdsComponent } from './shared/components/birds/birds.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingComponent,
+    ContactComponent,
+    ProjectsComponent,
+    BirdsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
